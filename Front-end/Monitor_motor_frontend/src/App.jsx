792a2ac35e2a6e7de7c228c7c639cf691ce109d2
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-01-21 12:36:20 +0700

    Frontend (add react router to frontend)

--- Front-end/Monitor_motor_frontend/src/App.jsx
+++ Front-end/Monitor_motor_frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Component/Home.jsx'
 import AboutUs from './Component/AboutUs'
 import SignIn from './Component/SignIn'
+import SignUp from "./Component/SignUp.jsx";
 
 function App() {
   return (
     <>
-      <Home />
-      <AboutUs />
-      <SignIn />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element ={<Home />}></Route>
+      <Route path="home" element ={<Home />}></Route>
+      <Route path="sign-in" element ={<SignIn />}></Route>
+      <Route path="/sign-up" element ={<SignUp />}></Route>
+      <Route path="/about-us" element ={<AboutUs />}></Route>
+      
+      </Routes>
+    </BrowserRouter> 
     </>
   )
 }

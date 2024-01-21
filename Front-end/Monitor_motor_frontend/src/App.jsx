e2ce4972980461295c9f442149924f189cd889fd
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-01-21 18:51:33 +0700

    Try clerk follow docs

--- Front-end/Monitor_motor_frontend/src/App.jsx
+++ Front-end/Monitor_motor_frontend/src/App.jsx
@@ -4,10 +4,19 @@ import AboutUs from "./Component/AboutUs";
 import SignIn from "./Component/SignIn";
 import SignUp from "./Component/SignUp.jsx";
 import Header from "./Component/Header.jsx";
+import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
 
 function App() {
   return (
     <>
+          <SignedOut>
+        <SignInButton />
+        <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton afterSignOutUrl="/" />
+        <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
+      </SignedIn>
       <BrowserRouter>
       <Header />
         <Routes>
